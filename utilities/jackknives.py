934438b06054c0d52cd0c26f4fc060af294f6fee
Thomas Kabelitz <thomas.kabelitz@adelaide.edu.au>
2024-06-27 11:29:23 +1000

    JackknifeEnsemble class now supports most mathematical operations as long as the broadcasting makes sense.

--- utilities/jackknives.py
+++ utilities/jackknives.py
@@ -5,23 +5,25 @@ import numpy as np
 
 # TODO: Update to allow 2nd order jackknives and also to allow multi-dim arrays
 
+
 class JackknifeEnsemble:
     def __init__(
         self, jackknives: np.ndarray, ensemble_average=None, jackknife_error=None
     ):
-        self.jackknives = jackknives
+        self.jackknives = (
+            jackknives if type(jackknives) is np.ndarray else np.array(jackknives)
+        )
         self.ncon = self.jackknives.size
         if ensemble_average is not None:
             self.ensemble_average = ensemble_average
         else:
-            self.ensemble_average = self._calculate_ensemble_average(jackknives)
+            self.ensemble_average = self._calculate_ensemble_average(self.jackknives)
         if jackknife_error is not None:
             self.jackknife_error = jackknife_error
         else:
             self.jackknife_error = self._calculate_jackknife_error(
-                jackknives, self.ensemble_average
+                self.jackknives, self.ensemble_average
             )
-
 
     @staticmethod
     def _calculate_ensemble_average(jackknives: np.ndarray):
@@ -65,42 +67,112 @@ class JackknifeEnsemble:
     @property
     def sum(self):
         return self.jackknives.sum()
-    
+
     def write_jackknives(self, filepath: os.PathLike, **kwargs):
         np.savetxt(filepath, self.jackknives, *kwargs)
-        
+
     @classmethod
     def from_file(cls, filepath: os.PathLike, **kwargs):
         jackknives = np.loadtxt(filepath, **kwargs)
         if jackknives.ndim != 1:
             raise ValueError("Contents of file must be one dimensional array.")
         return cls(jackknives)
-    
+
     def __add__(self, other):
-        jackknives = self.jackknives + other.jackknives
+        if type(other) is not type(self):
+            try:
+                jackknives = self.jackknives + other
+            except TypeError:
+                raise TypeError("Unsupported type for addition.")
+            except Exception as e:
+                raise e
+        else:
+            jackknives = self.jackknives + other.jackknives
         return JackknifeEnsemble(jackknives)
+
+    def __radd__(self, other):
+        return self + other
 
     def __sub__(self, other):
-        jackknives = self.jackknives - other.jackknives
-        return JackknifeEnsemble(jackknives)
+        return -1 * other + self
+
+    def __rsub__(self, other):
+        retur - self + other
 
     def __mul__(self, other):
-        jackknives = self.jackknives * other.jackknives
+        if type(other) is not type(self):
+            try:
+                jackknives = self.jackknives * other
+            except TypeError:
+                raise TypeError("Unsupported type for multiplication.")
+            except Exception as e:
+                raise e
+        else:
+            jackknives = self.jackknives * other.jackknives
         return JackknifeEnsemble(jackknives)
 
-    def __truediv__(self, other):
-        jackknives = self.jackknives / other.jackknives
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, denom):
+        return self * (1 / denom)
+
+    def __rtruediv__(self, numer):
+        if type(numer) is not type(self):
+            try:
+                jackknives = numer / self.jackknives
+            except TypeError:
+                raise TypeError("Unsupported type for division.")
+            except Exception as e:
+                raise e
+        else:
+            jackknives = numer.jackknives / self.jackknives
         return JackknifeEnsemble(jackknives)
 
-    def __rtruediv__(self, other):
-        jackknives = self.jackknives / other.jackknives
+    def __floordiv__(self, denom):
+        if type(denom) is not type(self):
+            try:
+                jackknives = self.jackknives // denom
+            except TypeError:
+                raise TypeError("Unsupported type for floor division.")
+            except Exception as e:
+                raise e
+        else:
+            jackknives = self.jackknives // denom.jackknives
         return JackknifeEnsemble(jackknives)
 
-    def __floordiv__(self, other):
-        jackknives = self.jackknives // other.jackknives
+    def __rfloordiv__(self, numer):
+        if type(numer) is not type(self):
+            try:
+                jackknives = numer // self.jackknives
+            except TypeError:
+                raise TypeError("Unsupported type for floor division.")
+            except Exception as e:
+                raise e
+        else:
+            jackknives = numer.jackknives // self.jackknives
         return JackknifeEnsemble(jackknives)
 
     def __pow__(self, pow):
-        jackknives = self.jackknives ** pow
+        if type(pow) is not type(self):
+            try:
+                jackknives = self.jackknives**pow
+            except TypeError:
+                raise TypeError("Unsupported type for exponentiation.")
+            except Exception as e:
+                raise e
+        else:
+            jackknives = self.jackknives**pow.jackknives
         return JackknifeEnsemble(jackknives)
 
+    def __rpow__(self, base):
+        if type(base) is not type(self):
+            try:
+                jackknives = base**self.jackknives
+            except TypeError:
+                raise TypeError("Unsupported type for exponentiation.")
+            except Exception as e:
+                raise e
+        else:
+            jackknives = base.jackknives**self.jackknives
+        return JackknifeEnsemble(jackknives)
