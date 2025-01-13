4c95a08410e163d30eabdc10438c0a527958b7fc
Thomas Kabelitz <thomas.kabelitz@adelaide.edu.au>
2025-01-13 14:07:34 +1100

    Updated jackknives object to support multiple dimension

--- tests/test_jackknives.py
+++ tests/test_jackknives.py
@@ -1,5 +1,7 @@
 import unittest
 
+import numpy as np
+
 from utilities import jackknives as jack
 
 
@@ -8,7 +10,7 @@ class Test_Jackknives(unittest.TestCase):
         ensemble = jack.JackknifeEnsemble([1, 1.01, 0.99, 0.95, 1.05])
         self.assertAlmostEqual(ensemble.ensemble_average, 1)
         self.assertAlmostEqual(ensemble.jackknife_error, 0.0806, places=4)
-        
+
     def test_addition(self):
         ensemble1 = jack.JackknifeEnsemble([1, 1.01, 0.99, 0.95, 1.05])
         ensemble2 = jack.JackknifeEnsemble([1, 1.01, 0.99, 0.95, 1.05])
@@ -17,7 +19,7 @@ class Test_Jackknives(unittest.TestCase):
         self.assertAlmostEqual(ensemble3.jackknife_error, 0.16127, places=4)
         self.assertAlmostEqual((ensemble1 + 1).ensemble_average, 2)
         self.assertAlmostEqual((ensemble1 + 1).jackknife_error, 0.0806, places=4)
-        
+
     def test_subtraction(self):
         ensemble1 = jack.JackknifeEnsemble([1, 1.01, 0.99, 0.95, 1.05])
         ensemble2 = jack.JackknifeEnsemble([2, 2.02, 1.98, 1.9, 2.1])
@@ -26,7 +28,7 @@ class Test_Jackknives(unittest.TestCase):
         self.assertAlmostEqual(ensemble3.jackknife_error, 0.0806, places=4)
         self.assertAlmostEqual((ensemble1 - 1).ensemble_average, 0)
         self.assertAlmostEqual((ensemble1 - 1).jackknife_error, 0.0806, places=4)
-        
+
     def test_multiplication(self):
         ensemble1 = jack.JackknifeEnsemble([1, 1.01, 0.99, 0.95, 1.05])
         ensemble2 = jack.JackknifeEnsemble([1, 1.01, 0.99, 0.95, 1.05])
@@ -35,7 +37,7 @@ class Test_Jackknives(unittest.TestCase):
         self.assertAlmostEqual(ensemble3.jackknife_error, 0.16127, places=4)
         self.assertAlmostEqual((ensemble1 * 2).ensemble_average, 2)
         self.assertAlmostEqual((ensemble1 * 2).jackknife_error, 0.16127, places=4)
-        
+
     def test_division(self):
         ensemble1 = jack.JackknifeEnsemble([1, 1.01, 0.99, 0.95, 1.05])
         ensemble2 = jack.JackknifeEnsemble([1, 1.01, 0.99, 0.95, 1.05])
@@ -46,14 +48,57 @@ class Test_Jackknives(unittest.TestCase):
         self.assertAlmostEqual((ensemble1 / 2).jackknife_error, 0.0403, places=4)
         self.assertAlmostEqual((1 / ensemble1).ensemble_average, 1.00104, places=4)
         self.assertAlmostEqual((1 / ensemble1).jackknife_error, 0.08087, places=4)
-        
+
     def test_power(self):
         ensemble1 = jack.JackknifeEnsemble([1, 1.01, 0.99, 0.95, 1.05])
         ensemble2 = jack.JackknifeEnsemble([1, 1.01, 0.99, 0.95, 1.05])
-        ensemble3 = ensemble1 ** ensemble2
+        ensemble3 = ensemble1**ensemble2
         self.assertAlmostEqual(ensemble3.ensemble_average, 1.00104, places=4)
         self.assertAlmostEqual(ensemble3.jackknife_error, 0.08077, places=4)
-        self.assertAlmostEqual((ensemble1 ** 2).ensemble_average, 1.0010, places=4)
-        self.assertAlmostEqual((ensemble1 ** 2).jackknife_error, 0.16127, places=4)
-        self.assertAlmostEqual((2 ** ensemble1).ensemble_average, 2.0004997, places=4)
-        self.assertAlmostEqual((2 ** ensemble1).jackknife_error, 0.111797, places=4)
+        self.assertAlmostEqual((ensemble1**2).ensemble_average, 1.0010, places=4)
+        self.assertAlmostEqual((ensemble1**2).jackknife_error, 0.16127, places=4)
+        self.assertAlmostEqual((2**ensemble1).ensemble_average, 2.0004997, places=4)
+        self.assertAlmostEqual((2**ensemble1).jackknife_error, 0.111797, places=4)
+
+    def test_get_jackknives(self):
+        data = np.array([1, 1.01, 0.99, 0.95, 1.05])
+        jackknives = jack.JackknifeEnsemble.get_jackknives(data)
+        self.assertTrue(
+            np.isclose(
+                jackknives, [1.0000, 0.9975, 1.0025, 1.0125, 0.9875], atol=1e-4
+            ).all()
+        )
+
+    def test_get_jackknives_multidim(self):
+        # fmt: off
+        data = np.array(
+            [[ 1.0, 1.01, 0.99, 0.95, 1.0500],
+             [ 1.0, 2.0,  1.5,  0.95, 1.2   ],
+             [-0.1, 1.3,  0.7,  0.2, -1.1001]]
+        )
+        result = np.array(
+            [[1.0,     0.9975, 1.0025, 1.0125, 0.9875],
+             [1.4125,  1.1625, 1.2875, 1.425,  1.3625],
+             [ 0.275, -0.075,  0.075,  0.2,    0.525]]
+        )
+        # fmt: on
+        self.assertTrue(
+            np.isclose(jack.JackknifeEnsemble.get_jackknives(data,axis=1), result, atol=1e-4).all()
+        )
+
+    def test_init_from_data(self):
+        # fmt: off
+        data = np.array(
+            [[ 1.0, 1.01, 0.99, 0.95, 1.0500],
+             [ 1.0, 2.0,  1.5,  0.95, 1.2   ],
+             [-0.1, 1.3,  0.7,  0.2, -1.1001]]
+        )
+        result = np.array(
+            [[1.0,     0.9975, 1.0025, 1.0125, 0.9875],
+             [1.4125,  1.1625, 1.2875, 1.425,  1.3625],
+             [ 0.275, -0.075,  0.075,  0.2,    0.525]]
+        )
+        jackknife_errors = np.array([0.016125, 0.193391, 0.402492])
+        j = jack.JackknifeEnsemble.init_jackknives_from_data(data, axis=1)
+        self.assertTrue(np.isclose(j.jackknives, result, atol=1e-4).all())
+        self.assertTrue(np.isclose(j.jackknife_error, jackknife_errors, atol=1e-4).all())
--- utilities/jackknives.py
+++ utilities/jackknives.py
@@ -1,81 +1,122 @@
 import functools
 import os
-
+import traceback
 import numpy as np
 
 # TODO: Update to allow 2nd order jackknives and also to allow multi-dim arrays
+# TODO: Shouldn't allow passing of average and error in constructor, allows for bugs if passed incorrectly
+#       and the speedup through not recalculating is essentially zero compared to all of the other things
+#       that need to happen as part of the constructor
 
 
 class JackknifeEnsemble:
     def __init__(
-        self, jackknives: np.ndarray, ensemble_average=None, jackknife_error=None
+        self,
+        jackknives: np.ndarray,
+        ensemble_average=None,
+        jackknife_error=None,
+        axis: int = 0,
     ):
+        if ensemble_average is not None and jackknife_error is not None:
+            print("WARNING: ensemble_average and jackknife_error are passed to constructor. PLEASE CHANGE FOR FUTURE, STACK TRACE FOR REFERENCE:")
+            traceback.print_stack()
         self.jackknives = (
             jackknives if type(jackknives) is np.ndarray else np.array(jackknives)
         )
-        self.ncon = self.jackknives.size
+        try:
+            self.ncon = self.jackknives.shape[axis]
+        except IndexError:
+            raise ValueError(
+                f"Axis {axis} out of bounds for array of shape {self.jackknives.shape}"
+            )
+        self.axis = axis
+
         if ensemble_average is not None:
             self.ensemble_average = ensemble_average
         else:
-            self.ensemble_average = self._calculate_ensemble_average(self.jackknives)
+            self.ensemble_average = self._calculate_ensemble_average(
+                self.jackknives, self.axis
+            )
         if jackknife_error is not None:
             self.jackknife_error = jackknife_error
         else:
             self.jackknife_error = self._calculate_jackknife_error(
-                self.jackknives, self.ensemble_average
+                self.jackknives, self.ensemble_average, axis=axis
             )
 
-    @staticmethod
-    def _calculate_ensemble_average(jackknives: np.ndarray):
-        return jackknives.mean()
+    @classmethod
+    def init_jackknives_from_data(cls, data: np.ndarray, axis: int = 0):
+        jackknives = cls.get_jackknives(data, axis)
+        return cls(jackknives, axis=axis)
 
     @staticmethod
-    def _calculate_jackknife_error(jackknives: np.ndarray, ensemble_average=None):
+    def _calculate_ensemble_average(jackknives: np.ndarray, axis: int = 0):
+        return jackknives.mean(axis=axis)
+
+    @staticmethod
+    def _calculate_jackknife_error(
+        jackknives: np.ndarray, ensemble_average=None, axis: int = 0
+    ):
+        ncon = jackknives.shape[axis]
         if ensemble_average is None:
-            ensemble_average = JackknifeEnsemble._calculate_ensemble_average(jackknives)
+            ensemble_average = JackknifeEnsemble._calculate_ensemble_average(
+                jackknives, axis=axis
+            )
         return np.sqrt(
-            np.sum((jackknives - ensemble_average) ** 2)
-            * jackknives.size
-            / (jackknives.size - 1)
+            np.sum((jackknives - broadcast(ensemble_average, axis=axis)) ** 2,axis=axis)
+            / ncon
+            * (ncon - 1)
         )
 
-    # TODO make this static
-    def _calculate_uncertainties(self) -> np.ndarray:
-        ncon = self.ncon
+    @staticmethod
+    def _calculate_uncertainties(data: np.ndarray, axis: int = 0) -> np.ndarray:
+        ncon = data.shape[axis]
         variances = np.zeros(ncon)
-        ensemble_squared = self.jackknives**2
+        ensemble_squared = data**2
 
-        sum_of_squares_term = self.sum_of_squares - ensemble_squared
+        sum_of_squares_term = (
+            broadcast((data**2).sum(axis=axis), axis=axis) - ensemble_squared
+        )
         squared_sum_term = (
-            self.square_sum + ensemble_squared - 2 * self.jackknives * self.sum
+            broadcast(data.sum(axis=axis) ** 2, axis=axis)
+            + ensemble_squared
+            - 2 * data * broadcast(data.sum(axis=axis), axis=axis)
         ) / (ncon - 1)
         variances = (ncon - 2) / (ncon - 1) * (sum_of_squares_term - squared_sum_term)
         return np.sqrt(variances)
 
+    @staticmethod
+    def get_jackknives(data: np.ndarray, axis: int = 0):
+        return (broadcast(data.sum(axis=axis), axis=axis) - data) / (
+            data.shape[axis] - 1
+        )
+
     @functools.cached_property
     def uncertainties(self):
-        return self._calculate_uncertainties()
+        return self._calculate_uncertainties(self.jackknives, axis=self.axis)
 
     @property
     def square_sum(self):
-        return self.jackknives.sum() ** 2
+        return self.jackknives.sum(axis=self.axis) ** 2
 
     @property
     def sum_of_squares(self):
-        return (self.jackknives**2).sum()
+        return (self.jackknives**2).sum(axis=self.axis)
 
     @property
     def sum(self):
-        return self.jackknives.sum()
+        return self.jackknives.sum(axis=self.axis)
 
     def write_jackknives(self, filepath: os.PathLike, **kwargs):
+        if len(self.jackknives.shape) > 1:
+            raise NotImplementedError("Only one dimensional arrays are supported for file writing.")
         np.savetxt(filepath, self.jackknives, *kwargs)
 
     @classmethod
     def from_file(cls, filepath: os.PathLike, **kwargs):
         jackknives = np.loadtxt(filepath, **kwargs)
         if jackknives.ndim != 1:
-            raise ValueError("Contents of file must be one dimensional array.")
+            raise NotImplementedError("Only one dimensional arrays are supported for file reading.")
         return cls(jackknives)
 
     def __add__(self, other):
@@ -176,3 +217,16 @@ class JackknifeEnsemble:
         else:
             jackknives = base.jackknives**self.jackknives
         return JackknifeEnsemble(jackknives)
+
+
+def broadcast(array: np.ndarray, axis: int):
+    try:
+        if len(array.shape) > axis+1:
+            raise ValueError(
+                f"Cannot broadcast to axis {axis} of array with shape {array.shape}"
+            )
+    except AttributeError:
+        return array
+    new_shape = list(array.shape)
+    new_shape.insert(axis, 1)
+    return array.reshape(new_shape)
